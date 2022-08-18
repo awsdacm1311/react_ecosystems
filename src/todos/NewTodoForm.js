59f3b380c69c61029a6f672f9f3f75d7324a4006
DanielC <dacm1311@hotmail.com>
2022-08-17 21:03:08 -0500

    adding new configuration to NewTodoForm

--- src/todos/NewTodoForm.js
+++ src/todos/NewTodoForm.js
@@ -1,18 +1,31 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { connect } from 'react-redux';
+import { createTodo } from "./actions";
 import './NewTodoForm.css'
 
-const NewTodoForm = () => {
+const NewTodoForm = ({todos, onCreatePressed}) => {
     const [inputValue, setInputValue] = useState('');
     return (
-    <div className="new-todo-form">
-        <input className="new-todo-input" 
+        <div className="new-todo-form">
+            <input className="new-todo-input"
                 type="text"
                 value={inputValue}
                 placeholder="Type your todo list"
-                onChange={(e)=>setInputValue(e.target.value)}/>
-        <button className="new-todo-button">Create todo</button>
-    </div>
-)
-    }
+                onChange={(e) => setInputValue(e.target.value)} />
+            <button onClick={()=>{
+                onCreatePressed(inputValue);
+                setInputValue("")
+            }}
+            className="new-todo-button">Create todo</button>
+        </div>
+    )
+}
 
-export default NewTodoForm;
+const mapStateToProps = state => ({
+    todos: state.todos,
+});
+const mapDispatchToProps = dispatch => ({
+    onCreatePressed: text => dispatch(createTodo(text)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewTodoForm);
